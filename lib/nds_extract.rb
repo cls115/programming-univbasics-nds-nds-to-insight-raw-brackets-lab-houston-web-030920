cb877e3cecab68b69d1685024ca927cbe289e022
Colton stephens <stephens.colton@icloud.com>
2020-02-27 03:00:10 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,26 +1,40 @@
-@@ -7,6 +7,7 @@ def directors_totals(nds)
+require 'pp'
+require 'pry'
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'directors_database'
+
+def directors_totals(nds)
+
+  hash = {}
+  row_index = 0 
+  while row_index < directors_database.length do
+   name = directors_database[row_index][:name]
+   hash[name] = 0 
+  movie_index = 0 
+  while movie_index < directors_database[row_index][:movies].length do 
+
+   hash[name] += directors_database[row_index][:movies][movie_index][:worldwide_gross]
+   movie_index += 1 
+ end
+ row_index += 1 
+ end
+ return hash 
+  #  pp directors_database[0][:movies][0][:worldwide_gross]
+  # Remember, it's always OK to pretty print what you get *in* to make sure
+  # that you know what you're starting with!
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
-  directors_index = 0
   result = {
   }
   #
-@ -18,5 +19,16 @@ def directors_totals(nds)
+  # Use loops, variables and the accessing method, [], to loop through the NDS
+  # and total up all the
+  # ...
+  # ...
+  # ...
   #
   #
   # Be sure to return the result at the end!
   nil
-  while directors_index < nds.length do
-    directors_name = nds[directors_index][:name]
-    result[directors_name] = 0 
-    movie_index = 0 
-
-    while movie_index < nds[directors_index][:movies].length do
-      result[directors_name] += nds[directors_index][:movies][movie_index][:worldwide_gross]
-      movie_index += 1 
-    end
-    directors_index += 1
-  end
-  result
 end
